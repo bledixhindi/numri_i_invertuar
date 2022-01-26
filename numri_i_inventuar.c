fe218ee7a25b2dd4a44e47b2ca2661a6f6a3ad19
bledixhindi <54402850+bledixhindi@users.noreply.github.com>
2022-01-26 18:25:20 +0100

    Add files via upload

---
+++ numri_i_inventuar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int n, rev = 0, remainder;
+    printf("Vendos numrin: ");
+    scanf("%d", &n);
+    while (n != 0) {
+        remainder = n % 10;
+        rev = rev * 10 + remainder;
+        n /= 10;
+    }
+    printf("Numri i invertuara= %d", rev);
+    return 0;
+}
+
